150be675dca075d54c508beb55135b904e4a35af
usama <usamabinattique@gmail.com>
2021-05-07 21:30:28 +0500

    Home presenter implementation for converting response to view model

--- The Hitchhiker Prophecy/Modules/Home/Home Scene/HomeScenePresenter.swift
+++ The Hitchhiker Prophecy/Modules/Home/Home Scene/HomeScenePresenter.swift
@@ -16,6 +16,29 @@ class HomeScenePresneter: HomeScenePresentationLogic {
     }
     
     func presentCharacters(_ response: HomeScene.Search.Response) {
-        
+        switch response {
+        case let .success(output):
+            
+            displayView?.didFetchCharacters(viewModel: output.data.results.map(convertOutputToVM(_:)))
+            
+        case let .failure(error):
+            displayView?.failedToFetchCharacters(error: error)
+        }
+    }
+}
+
+private extension HomeScenePresneter {
+    func convertOutputToVM(_ character: Characters.Search.Character) -> HomeScene.Search.ViewModel {
+        HomeScene.Search.ViewModel(name: character.name,
+                                   desc: character.resultDescription,
+                                   imageUrl: imageUrl(thumbanail: character.thumbnail),
+                                   comics: character.comics.collectionURI,
+                                   series: character.series.collectionURI,
+                                   stories: character.stories.collectionURI,
+                                   events: character.events.collectionURI)
+    }
+    
+    func imageUrl(thumbanail: Characters.Search.Character.Thumbnail) -> String {
+        String(format: "%@.%@", arguments: [thumbanail.path, thumbanail.thumbnailExtension])
     }
 }
